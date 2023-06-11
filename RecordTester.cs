e80b8fb77d660c1031fddefdf45373101b930201
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-10 22:55:19 -0400

    Add new properties to test the base ToString() override

--- RecordTester.cs
+++ RecordTester.cs
@@ -5,21 +5,11 @@ public record firstRecord
     public firstRecord(Dictionary<string, int> menu)
     {   
         Menu = menu;
+        dogHatFoodDude = "Woot door bro";
+        hoursOfSexStraight = 12;
     }   
 
     public Dictionary<string, int> Menu {get; init;}
-
-
-    public override string ToString()
-    {
-        string output = "";
-
-        foreach (KeyValuePair<string, int> kvp in Menu)
-        {
-            output += $"${kvp.Value} - {kvp.Key}" + "\n";
-        }
-
-        return output;
-    }
-   
+    public string dogHatFoodDude {get; init;}
+    public int hoursOfSexStraight {get; init;}   
 }
