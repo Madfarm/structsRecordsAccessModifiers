e92f565f54a445b406229c986bb2843f2717c374
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-10 22:01:49 -0400

    Add struct definition

---
+++ StructTester.cs
@@ -0,0 +1,14 @@
+namespace structGo;
+
+
+public struct firstStruct 
+{
+    public firstStruct(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public string Name { get; set;}
+    public int Age { get; set;}
+}
