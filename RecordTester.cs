58b8f88b7bf720e6635184881c8134e8cd6183b0
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-10 22:58:13 -0400

    Revert to the old ToString override

--- RecordTester.cs
+++ RecordTester.cs
@@ -5,11 +5,22 @@ public record firstRecord
     public firstRecord(Dictionary<string, int> menu)
     {   
         Menu = menu;
-        dogHatFoodDude = "Woot door bro";
-        hoursOfSexStraight = 12;
+        // dogHatFoodDude = "Woot door bro";
+        // hoursOfSexStraight = 12;
     }   
 
     public Dictionary<string, int> Menu {get; init;}
-    public string dogHatFoodDude {get; init;}
-    public int hoursOfSexStraight {get; init;}   
+    // public string dogHatFoodDude {get; init;}
+    // public int hoursOfSexStraight {get; init;}
+
+    public override string ToString()
+    {
+        string output = "Our Menu\n\n";
+        foreach(KeyValuePair<string, int> kvp in Menu)
+        {
+            output += $"${kvp.Value} - {kvp.Key}" + "\n";
+        }
+
+       return output;
+    }
 }
